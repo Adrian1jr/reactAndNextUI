0f655f2c5b034cff46686d3425926c1ffa3a8780
Adrian2jr <adrian.tapia@leadonemx.com>
2024-01-28 14:04:39 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Test from "./components/Test";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline">Hello World</h1>
+
+      <Test />
+    </>
+  );
+}
+
+export default App;
